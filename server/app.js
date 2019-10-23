2f1b82d3bc1e920b610bca36b1e398af8f6769e3
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-10-23 15:28:18 -0700

    sets up express, added start script, added morgan and axios

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+const axios = require('axios');
 
 
 const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.status(200).send('hello world');
+});
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
