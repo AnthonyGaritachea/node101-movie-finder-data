2c9c8f71854172590c0ad28fbc6fac45d035c5c9
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-10-25 18:04:50 -0700

    project done

--- server/app.js
+++ server/app.js
@@ -2,15 +2,25 @@ const express = require('express');
 const morgan = require('morgan');
 const axios = require('axios');
 
+let cache = {};
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan('dev'));    
 
 app.get('/', (req, res) => {
-    res.status(200).send('hello world');
-});
-
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+    if(!cache[req.url]) {
+       axios.get('http://www.omdbapi.com' + req.url + '&apikey=8730e0e')
+       .then(response =>  {
+           cache[req.url] = response.data;
+           res.send(response.data);
+        //    console.log(JSON.stringify(cache))
+       })
+       .catch(error => res.send(error.message)); 
+    } else {
+        res.send(cache[req.url]);
+        // console.log(JSON.stringify(cache))
+    }
+   });
 
 module.exports = app;
